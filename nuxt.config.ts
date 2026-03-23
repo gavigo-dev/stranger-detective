de4e3287c6c468edb90835e24338cfe5f2d656ad
gavigo.dev <gavigo.dev@gmail.com>
2026-03-23 01:45:18 -0300

    fix: force manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,10 @@ export default defineNuxtConfig({
                 {
                     rel: 'stylesheet',
                     href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+                },
+                {
+                    rel: 'manifest',
+                    href: '/manifest.webmanifest'
                 }
             ]
         }
